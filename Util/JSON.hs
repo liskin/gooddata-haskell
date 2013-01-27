0fa4161434924ee60f0ed3c0c4f53809962a62db
Tomas Janousek <tomi@nomi.cz>
2013-01-27 16:39:11 +0100

    Util.JSON: Add untag and optionalPropBy.

--- Util/JSON.hs
+++ Util/JSON.hs
@@ -6,8 +6,8 @@ module Util.JSON
     ( module Control.Category
     , module Language.JsonGrammar
     , module Data.Iso
-    , tag
-    , optionalProp
+    , tag, untag
+    , optionalProp, optionalPropBy
     ) where
 
 import Data.Aeson ( Value, Object )
@@ -20,5 +20,11 @@ import Language.JsonGrammar
 tag ∷ String → Iso (Value :- t) (Value :- t)
 tag t = object (prop t)
 
+untag ∷ String → Iso (Value :- t) (Value :- t)
+untag t = inverse (tag t)
+
 optionalProp ∷ ( Json a ) ⇒ String → Iso (Object :- t) (Object :- Maybe a :- t)
 optionalProp name = duck just . prop name <> duck nothing
+
+optionalPropBy ∷ Iso (Value :- t) (a :- t) → String → Iso (Object :- t) (Object :- Maybe a :- t)
+optionalPropBy g name = duck just . propBy g name <> duck nothing
